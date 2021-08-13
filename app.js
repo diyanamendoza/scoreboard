aea742a148bf14b4415d0d324beb9c02f6880488
diyanamendoza <diyana.amp@pm.me>
2021-08-13 13:51:11 -0700

    Add html, js, basic styling

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import functions and grab DOM elements
-
+const scoreOne = document.querySelector('#score-1');
+const scoreTwo = document.querySelector('#score-2');
+const ducksButton = document.querySelector('#team-1');
+const pigeonsButton = document.querySelector('#team-2');
 // initialize state
 
+let ducksScore = 0;
+let pigeonsScore = 0;
+
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+ducksButton.addEventListener('click', () => {
+  ducksScore = ducksScore + 7;
+  scoreOne.textContent = ducksScore;
+} 
+)
+
+pigeonsButton.addEventListener('click', () => {
+  pigeonsScore = pigeonsScore + 7;
+  scoreTwo.textContent = pigeonsScore;
+} 
+)
